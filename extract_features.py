d2a8721c353aade6f9d6c81935afdbfa03f54376
Yu Jiang <jiangyu@hermesconrad.ucr.edu>
2018-05-21 22:12:13 -0700

    1

---
+++ extract_features.py
@@ -0,0 +1,43 @@
+import os
+import numpy as np
+import tensorflow as tf
+import cv2
+import scipy.io as sio
+import glob
+import numpy as np
+import tensorflow.keras.preprocessing.image
+''' Works with tensorflow >= 1.5 '''
+
+# SPECIFY PATH TO THE DATASET
+path_to_dataset = '/data/ece243/tiny-UCF101'
+
+def main():
+
+  feature = []
+  label = []
+  categories = sorted(os.listdir(path_to_dataset))
+
+  # FILL IN TO LOAD THE ResNet50 MODEL
+  resnet = tf.keras.applications.ResNet50(weights = 'imagenet')
+
+  for i,c in enumerate(categories):
+    path_to_images = sorted(glob.glob(os.path.join(path_to_dataset,c) + '/*.jpg'))
+    for p in path_to_images:
+      # FILL IN TO LOAD IMAGE, PREPROCESS, EXTRACT FEATURES. 
+      # OUTPUT VARIABLE F EXPECTED TO BE THE FEATURE OF THE IMAGE OF DIMENSION (2048,)
+      img = image.load_img(path_to_image, target_size=(224,224))
+      x = image.img_to_array(img)
+      x = np.expand_dims(x, axis = 0)
+      x = preprocess_input(x)
+      
+      F = resnet.predict(x)
+
+      feature.append(F)
+      label.append(categories.index(c))
+      print(np.shape(feature))
+
+  sio.savemat('ucf101dataset.mat', mdict={'feature': feature, 'label': label})
+
+
+if __name__ == "__main__":
+   main()
